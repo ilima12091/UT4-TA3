91156b91e0ee54bc605f109463d7c1f1d3889f6d
JuanCampos1 <juan.campos@correo.ucu.edu.uy>
2023-05-17 21:08:12 -0300

    Created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavBarItemsComponent } from './nav-bar/nav-bar-items/nav-bar-items.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    NavBarItemsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule, 
+    AngularSvgIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/nav-bar/nav-bar-items/nav-bar-items.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar-items',
+  templateUrl: './nav-bar-items.component.html',
+  styleUrls: ['./nav-bar-items.component.css']
+})
+export class NavBarItemsComponent {
+  
+}
